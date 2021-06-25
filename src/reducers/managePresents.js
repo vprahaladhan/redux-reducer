351748a0a82127b0e401d3a3f92b4bb771426252
prahal <vprahaladhan@gmail.com>
2021-06-25 21:09:40 +1000

    Done.

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,6 @@
-export function managePresents(state, action){
+export function managePresents(state = {numberOfPresents: 0}, action){
+  if (action.type === 'INCREASE') {
+    return {numberOfPresents: state.numberOfPresents + 1}
+  }
+  else return state;
 }
