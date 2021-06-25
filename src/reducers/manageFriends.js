5846b54d1e76769dc551151816bb53c7cd6d9c71
prahal <vprahaladhan@gmail.com>
2021-06-25 20:31:31 +1000

    commit tree 6d688e0ffca780e5a0296aa17cd25acbb2de1c17

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,11 @@
-export function manageFriends(state, action){
+export function manageFriends(state = {friends: []}, action) {
+  switch(action.type) {
+    case 'ADD_FRIEND': return {...state, friends: [...state.friends].concat(action.friend)}
+    case 'REMOVE_FRIEND': 
+      const index = state.friends.indexOf(state.friends.find(e => e.id === action.id))
+      let new_friends = [...state.friends]
+      new_friends.splice(index, 1)
+      return {...state, friends: new_friends}
+    default: return state
+  }
 }
